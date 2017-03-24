60d633294559398f25b28b0d7bfd06c06c7531d8
Joackim Balouka <jobalouka@gmail.com>
2017-03-24 11:56:14 +0100

    last modif

--- src/client/app/scenes/home/Home.jsx
+++ src/client/app/scenes/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Header from '../../components/header/Header.jsx';
 
 
 class Home extends React.Component {
 	render() {
-
+		return(
+			<Header />
+		)
 	}
 }
 
