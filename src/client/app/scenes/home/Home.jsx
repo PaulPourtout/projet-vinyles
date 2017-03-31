faac4200aaa55de7e64521c32f2555ee1a0ace62
Adamska <adams.roy27@gmail.com>
2017-03-31 17:32:56 +0200

    ajout component footer, contact, modif description

--- src/client/app/scenes/home/Home.jsx
+++ src/client/app/scenes/home/Home.jsx
@@ -3,7 +3,8 @@ import {render} from 'react-dom';
 import Header from './../../components/header/Header.jsx';
 import NewRecords from './../../components/newRecords/NewRecords.jsx';
 import Description from '../../components/description/Description.jsx';
-
+import Footer from '../../components/footer/Footer.jsx';
+import Contact from '../../components/contact/Contact.jsx';
 
 class Home extends React.Component {
   render() {
@@ -12,6 +13,8 @@ class Home extends React.Component {
         <Header/>
         <NewRecords/>
 				<Description />
+        <Contact />
+        <Footer />
       </div>
     )
   }
